363cea18abc375c6a6906412e098d2c9c9121ff1
akshay waghmare <akshay.d.waghmare@gmail.com>
2025-08-23 00:27:50 +0530

    fix(ssr): avoid duplicate server hydration by removing withEventReplay() from client config in app.config.ts

--- frontend/apps/console-app/src/app/app.config.ts
+++ frontend/apps/console-app/src/app/app.config.ts
@@ -2,12 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), 
+    provideClientHydration()
   ]
 };
